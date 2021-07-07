3d721303071a6b33fc8c8bd114cc3a324b1d26ef
koojin17 <81981155+koojin17@users.noreply.github.com>
2021-07-07 23:37:20 +0300

    Update kaynak.cpp

--- kaynak.cpp
+++ kaynak.cpp
@@ -4,17 +4,17 @@
 int main()
 {
 	HMODULE hModule = GetModuleHandle(NULL); 
-	HRSRC hResource = FindResourceW(hModule, MAKEINTRESOURCEW(101), L"CPP"); // resource'a eklenmiþ dosyamýzýn konumu
+	HRSRC hResource = FindResourceW(hModule, MAKEINTRESOURCEW(101), L"CPP"); // resource'a eklenmis dosyamizin konumu
 
 
 	HGLOBAL hMemory = LoadResource(hModule, hResource);   //resource' u yüklesin
-	DWORD dwSize = SizeofResource(hModule, hResource);   //resource un boyutu
-	LPVOID lpAddress = LockResource(hMemory);			//LockResource' un dönüþ deðeri,resourceumuzdaki dosyanýn ilk baytýnýn iþaretçisidir.
+	DWORD dwSize = SizeofResource(hModule, hResource);   //resource' un boyutu
+	LPVOID lpAddress = LockResource(hMemory);			//LockResource' un dönüs degeri,resourceumuzdaki dosyanýn ilk baytýnýn iþaretçisidir.
 
 	HANDLE hFile;
 	DWORD dwBytesWritten = 0;
 	std::string a = "hello.exe";
-	hFile = CreateFileA((LPCSTR)a.c_str(),  // name of the write
+	hFile = CreateFileA((LPCSTR)a.c_str(),  //isim ve yol belirle
 		GENERIC_WRITE,          
 		0,                     
 		NULL,                   
